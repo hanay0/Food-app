c3f85d125a6fe62b883314c6368b613bf1ac531a
hanay0 <ahanafy90@yahoo.com>
2023-07-14 18:47:39 +0300

    implementing header and cart button

---
+++ src/Layout/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import HeaderCartButton from '../Layout/HeaderCartButton';
+
+import mealsImg from '../assets/meals.jpg';
+import classes from './Header.module.css';
+
+
+// header component will hold the header itself and the background image below the header
+// it can be splitted to 2 separate components (header & hero banner)
+
+ const Header = props => {
+  return (
+    <React.Fragment>
+        <header className={classes.header}>
+            <h1>React Meals</h1>
+            <HeaderCartButton/>
+        </header>
+        <div className={classes['main-image']}>
+            <div className={classes.overlay}></div>
+            <img src={mealsImg} alt='food table'/>
+        </div>
+    </React.Fragment>
+    )
+};
+
+export default Header
